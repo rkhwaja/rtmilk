89ddb47453bef33da4e3fef47a3b26c782ef19c5
Rehan Khwaja <rehan@khwaja.name>
2021-05-03 00:09:17 -0700

    Add some pydantic models
    All tests pass

--- src/rtm/api.py
+++ src/rtm/api.py
@@ -1,7 +1,12 @@
 from hashlib import md5
 from logging import getLogger
+from pprint import pformat
+from typing import List
 
+from pydantic import validate_arguments, ValidationError
 from requests import get
+
+from .models import AuthCheckTokenResponse, FailStat, ListsGetListResponse, NotesResponse, SettingsGetListResponse, TagsGetListResponse, TasksGetListResponse, TasksResponse, TaskTagsResponse, TestEchoResponse, TimelinesCreateResponse
 
 _restUrl = 'https://api.rememberthemilk.com/services/rest/'
 _log = getLogger('rtm')
@@ -21,13 +26,25 @@ def _ApiSig(sharedSecret, params):
 	concatenatedParams = ''.join((key + value for key, value in sortedItems))
 	return md5((sharedSecret + concatenatedParams).encode()).hexdigest()
 
+def _CheckKwargs(keywordArgs, allowedArgs):
+	for name, _ in keywordArgs.items():
+		if name not in allowedArgs:
+			raise TypeError(f'{name} is an unexpected keyword argument')
+
 class API:
 	"""
 	Low-level API wrapper
-	parameter names are the same as on the service itself
-	Wraps failures in exceptions
-	Checks validity of parameters to the extent possible
-	Returns the dictionary with the common parts removed (e.g. 'stat')
+	Handles the authorization/authentication token and API signature
+	There is (almost) a 1-1 relationship between API calls and public member functions
+	Parameter names are the same as the API
+	The inputs are strings at the moment
+	The outputs are parsed into pydantic types, including errors
+	The upper layer will:
+	- translate failures into exceptions
+	- translate inputs from python types to strings if we don't move it to this layer. Use pydantic for this
+	- organize stuff like setting start/due dates in the order that they will be valid
+	- hide details like timeline, filter format
+	- only send API calls for changed properties
 	"""
 	def __init__(self, apiKey, sharedSecret, storage):
 		self.apiKey = apiKey
@@ -43,47 +60,27 @@ class API:
 	def _MethodParams(self, methodName):
 		return {'method': methodName, 'api_key': self.apiKey, 'format': 'json', 'v': '2'}
 
-	def _CallUnauthorized(self, method, **params):
-		_log.info(f'_CallUnauthorized: {method}, {params}')
-		params.update(self._MethodParams(method))
+	def _Call(self, params):
 		params['api_sig'] = self._ApiSig(params)
-		rsp = get(_restUrl, params=params).json()['rsp']
-		stat = rsp['stat']
-		if stat == 'ok':
-			del rsp['stat']
-			return rsp
-		assert stat == 'fail', rsp
-		err = rsp['err']
-		raise RTMError(int(err['code']), err['msg'])
+		json = get(_restUrl, params=params).json()
+		_log.debug(f'JSON response:\n{pformat(json)}')
+		return json['rsp']
+
+	def _CallUnauthorized(self, method, **params):
+		_log.debug(f'_CallUnauthorized: {method}, {params}')
+		params.update(self._MethodParams(method))
+		return self._Call(params)
 
 	def _CallAuthorized(self, method, **params):
-		_log.info(f'_CallAuthorized: {method}, {params}')
+		_log.debug(f'_CallAuthorized: {method}, {params}')
 		params.update(self._MethodParams(method))
 		params.update({'auth_token': self.storage.Load()})
-		params['api_sig'] = self._ApiSig(params)
-		rsp = get(_restUrl, params=params).json()['rsp']
-		stat = rsp['stat']
-		if stat == 'ok':
-			del rsp['stat']
-			# _log.info(f'_CallAuthorized -> {rsp}')
-			return rsp
-		assert stat == 'fail', rsp
-		err = rsp['err']
-		raise RTMError(int(err['code']), err['msg'])
+		return self._Call(params)
 
 	def TestEcho(self, **params):
 		_log.info(f'Echo: {params}')
 		rsp = self._CallUnauthorized('rtm.test.echo', **params)
-		assert rsp['method'] == 'rtm.test.echo', rsp
-		del rsp['method']
-		assert rsp['api_key'] == self.apiKey, rsp
-		del rsp['api_key']
-		assert rsp['format'] == 'json', rsp
-		del rsp['format']
-		assert rsp['v'] == '2', rsp
-		del rsp['v']
-		del rsp['api_sig']
-		return rsp
+		return TestEchoResponse(**rsp)
 
 	def AuthGetFrob(self):
 		rsp = self._CallUnauthorized('rtm.auth.getFrob')
@@ -93,8 +90,32 @@ class API:
 		rsp = self._CallUnauthorized('rtm.auth.getToken', frob=frob)
 		return rsp['auth']['token']
 
-	def AuthCheckToken(self):
-		return self._CallAuthorized('rtm.auth.checkToken')
+	def AuthCheckToken(self, auth_token):
+		rsp = self._CallUnauthorized('rtm.auth.checkToken', auth_token=auth_token)
+		return AuthCheckTokenResponse(**rsp)
+
+	def ListsAdd(self, timeline, name, **kwargs):
+		_CheckKwargs(kwargs, ['filter']) # TODO validate parameter
+		return self._CallAuthorized('rtm.lists.add', timeline=timeline, name=name, **kwargs)
+
+	def ListsArchive(self, timeline, list_id):
+		return self._CallAuthorized('rtm.lists.archive', timeline=timeline, list_id=list_id)
+
+	def ListsDelete(self, timeline, list_id):
+		return self._CallAuthorized('rtm.lists.delete', timeline=timeline, list_id=list_id)
+
+	def ListsGetList(self):
+		rsp = self._CallAuthorized('rtm.lists.getList')
+		return ListsGetListResponse(**rsp)
+
+	def ListsSetDefaultList(self, timeline, list_id):
+		return self._CallAuthorized('rtm.lists.setDefaultList', timeline=timeline, list_id=list_id)
+
+	def ListsSetName(self, timeline, list_id, name):
+		return self._CallAuthorized('rtm.lists.setName', timeline=timeline, list_id=list_id, name=name)
+
+	def ListsUnarchive(self, timeline, list_id):
+		return self._CallAuthorized('rtm.lists.unarchive', timeline=timeline, list_id=list_id)
 
 	def PushGetSubscriptions(self):
 		return self._CallAuthorized('rtm.push.getSubscriptions')['subscriptions']
@@ -102,32 +123,47 @@ class API:
 	def PushGetTopics(self):
 		return self._CallAuthorized('rtm.push.getTopics')['topics']
 
-	def PushSubscribe(self, url, topics, push_format, timeline, **args):
-		# optional args are lease_seconds and filter
-		return self._CallAuthorized('rtm.push.subscribe', url=url, topics=topics, push_format=push_format, timeline=timeline, **args)
+	def PushSubscribe(self, url, topics, push_format, timeline, **kwargs):
+		_CheckKwargs(kwargs, ['lease_seconds', 'filter']) # TODO validate parameters
+		return self._CallAuthorized('rtm.push.subscribe', url=url, topics=topics, push_format=push_format, timeline=timeline, **kwargs)
 
 	def PushUnsubscribe(self, timeline, subscription_id):
 		self._CallAuthorized('rtm.push.unsubscribe', timeline=timeline, subscription_id=subscription_id)
 
 	def TimelinesCreate(self):
 		rsp = self._CallAuthorized('rtm.timelines.create')
-		return rsp['timeline']
+		return TimelinesCreateResponse(**rsp)
 
 	def SettingsGetList(self):
 		rsp = self._CallAuthorized('rtm.settings.getList')
-		return rsp['settings']
+		return SettingsGetListResponse(**rsp)
 
 	def TagsGetList(self):
-		return self._CallAuthorized('rtm.tags.getList')['tags']['tag']
+		rsp = self._CallAuthorized('rtm.tags.getList')
+		try:
+			return TagsGetListResponse(**rsp)
+		except ValidationError as e:
+			failStat = FailStat(**rsp)
+			raise RTMError(failStat.err.code, failStat.err.msg) from e
 
-	def TasksAdd(self, timeline, name, **args):
-		# optional args are list_id, parse (1 or not), parent_task_id, external_id
-		rsp = self._CallAuthorized('rtm.tasks.add', timeline=timeline, name=name, **args)
-		return rsp
+	def TasksAdd(self, timeline, name, **kwargs):
+		_CheckKwargs(kwargs, ['list_id', 'parse', 'parent_task_id', 'external_id']) # TODO validate parameters
+		rsp = self._CallAuthorized('rtm.tasks.add', timeline=timeline, name=name, **kwargs)
+		try:
+			return TaskTagsResponse(**rsp)
+		except ValidationError as e:
+			failStat = FailStat(**rsp)
+			raise RTMError(failStat.err.code, failStat.err.msg) from e
 
-	def TasksAddTags(self, timeline, list_id, taskseries_id, task_id, tags):
-		rsp = self._CallAuthorized('rtm.tasks.addTags', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, tags=tags)
-		return rsp
+	@validate_arguments
+	def TasksAddTags(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, tags: List[str]):
+		# TODO the join here should be done as part of the pydantic parameter validation stuff
+		rsp = self._CallAuthorized('rtm.tasks.addTags', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, tags=','.join(tags))
+		try:
+			return TaskTagsResponse(**rsp)
+		except ValidationError as e:
+			failStat = FailStat(**rsp)
+			raise RTMError(failStat.err.code, failStat.err.msg) from e
 
 	def TasksComplete(self, timeline, list_id, taskseries_id, task_id):
 		rsp = self._CallAuthorized('rtm.tasks.complete', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id)
@@ -135,36 +171,70 @@ class API:
 
 	def TasksDelete(self, timeline, list_id, taskseries_id, task_id):
 		rsp = self._CallAuthorized('rtm.tasks.delete', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id)
-		return rsp
+		try:
+			return TaskTagsResponse(**rsp)
+		except ValidationError as e:
+			failStat = FailStat(**rsp)
+			raise RTMError(failStat.err.code, failStat.err.msg) from e
 
-	def TasksGetList(self, **args):
-		# optional args are list_id, filter, last_sync
-		rsp = self._CallAuthorized('rtm.tasks.getList', **args)
-		return rsp['tasks']
+	def TasksGetList(self, **kwargs):
+		_CheckKwargs(kwargs, ['list_id', 'filter', 'last_sync']) # TODO validate parameters
+		rsp = self._CallAuthorized('rtm.tasks.getList', **kwargs)
+		try:
+			return TasksGetListResponse(**rsp)
+		except ValidationError as e:
+			failStat = FailStat(**rsp)
+			raise RTMError(failStat.err.code, failStat.err.msg) from e
 
-	def TasksMovePriority(self, timeline, list_id, taskseries_id, task_id, direction):
+	def TasksMovePriority(self, timeline, list_id, taskseries_id, task_id, direction): # TODO validate parameter
 		rsp = self._CallAuthorized('rtm.tasks.movePriority', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, direction=direction)
 		return rsp
 
-	def TasksRemoveTags(self, timeline, list_id, taskseries_id, task_id, tags):
+	@validate_arguments
+	def TasksNotesAdd(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, note_title: str, note_text: str):
+		rsp = self._CallAuthorized('rtm.tasks.notes.add', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, note_title=note_title, note_text=note_text)
+		try:
+			return NotesResponse(**rsp)
+		except ValidationError as e:
+			failStat = FailStat(**rsp)
+			raise RTMError(failStat.err.code, failStat.err.msg) from e
+
+	def TasksRemoveTags(self, timeline, list_id, taskseries_id, task_id, tags: List[str]):
 		rsp = self._CallAuthorized('rtm.tasks.removeTags', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, tags=tags)
 		return rsp
 
-	def TasksSetDueDate(self, timeline, list_id, taskseries_id, task_id, **args):
-		# optional args are due, has_due_time, parse
-		rsp = self._CallAuthorized('rtm.tasks.setDueDate', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, **args)
-		return rsp
+	@validate_arguments
+	def TasksSetDueDate(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, **kwargs):
+		_CheckKwargs(kwargs, ['due', 'has_due_time', 'parse']) # TODO validate parameters
+		rsp = self._CallAuthorized('rtm.tasks.setDueDate', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, **kwargs)
+		try:
+			return TasksResponse(**rsp)
+		except ValidationError as e:
+			failStat = FailStat(**rsp)
+			raise RTMError(failStat.err.code, failStat.err.msg) from e
 
-	def TasksSetName(self, timeline, list_id, taskseries_id, task_id, name):
+	def TasksSetName(self, timeline, list_id, taskseries_id, task_id, name: str):
 		rsp = self._CallAuthorized('rtm.tasks.setName', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, name=name)
 		return rsp
 
-	def TasksSetStartDate(self, timeline, list_id, taskseries_id, task_id, **args):
-		# optional args are start, has_start_time, parse
-		rsp = self._CallAuthorized('rtm.tasks.setStartDate', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, **args)
-		return rsp
+	def TasksSetPriority(self, timeline, list_id, taskseries_id, task_id, **kwargs):
+		_CheckKwargs(kwargs, ['priority']) # TODO validate parameter
+		return self._CallAuthorized('rtm.tasks.setPriority', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, **kwargs)['list']
 
-	def TasksSetTags(self, timeline, list_id, taskseries_id, task_id, **args):
-		# optional args are tags
-		rsp = self._CallAuthorized('rtm.tasks.setTags', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, **args)
-		return rsp
+	def TasksSetStartDate(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, **kwargs):
+		_CheckKwargs(kwargs, ['start', 'has_start_time', 'parse']) # TODO validate parameter
+		rsp = self._CallAuthorized('rtm.tasks.setStartDate', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, **kwargs)
+		try:
+			return TasksResponse(**rsp)
+		except ValidationError as e:
+			failStat = FailStat(**rsp)
+			raise RTMError(failStat.err.code, failStat.err.msg) from e
+
+	def TasksSetTags(self, timeline, list_id, taskseries_id, task_id, **kwargs):
+		_CheckKwargs(kwargs, ['tags']) # TODO validate parameter
+		rsp = self._CallAuthorized('rtm.tasks.setTags', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, **kwargs)
+		try:
+			return TaskTagsResponse(**rsp)
+		except ValidationError as e:
+			failStat = FailStat(**rsp)
+			raise RTMError(failStat.err.code, failStat.err.msg) from e
--- tests/test_api_wrapper.py
+++ tests/test_api_wrapper.py
@@ -4,22 +4,35 @@ from logging import info
 from dateutil.tz import gettz
 from pytest import raises
 
-from rtm import RTMError
+from rtm import AuthCheckTokenResponse, RTMError, TestEchoResponse
 
 def test_echo(api):
 	response = api.TestEcho(a='1', b='2')
-	assert response == {'a': '1', 'b': '2'}
+	assert isinstance(response, TestEchoResponse), response
 
 def test_check_token(api):
-	api.AuthCheckToken()
+	response = api.AuthCheckToken(api.storage.Load())
+	assert isinstance(response, AuthCheckTokenResponse)
 
-def test_add_and_delete_task(api):
+def test_add_and_delete_basic_task(api):
 	timeline = api.TimelinesCreate()
-	task = api.TasksAdd(timeline, 'test_add_and_delete_task')
+	task = api.TasksAdd(timeline.timeline, 'test_add_and_delete_basic_task')
 	api.TasksDelete(
-		timeline, task['list']['id'],
-		task['list']['taskseries'][0]['id'],
-		task['list']['taskseries'][0]['task'][0]['id'])
+		timeline.timeline, task.list.id,
+		task.list.taskseries[0].id,
+		task.list.taskseries[0].task[0].id)
+
+def test_add_and_delete_task_with_tags(api):
+	timeline = api.TimelinesCreate()
+	task = api.TasksAdd(timeline.timeline, 'test_add_and_delete_task_with_tags')
+
+	api.TasksAddTags(timeline.timeline, task.list.id, task.list.taskseries[0].id, task.list.taskseries[0].task[0].id, ['tag1'])
+	# assert 'tag1' in {x.name for x in api.TagsGetList().tags.tag}
+
+	api.TasksDelete(
+		timeline.timeline, task.list.id,
+		task.list.taskseries[0].id,
+		task.list.taskseries[0].task[0].id)
 
 def test_delete_non_existing_task(api, timeline):
 	with raises(RTMError):
@@ -28,56 +41,66 @@ def test_delete_non_existing_task(api, timeline):
 			'43', '')
 
 def test_tags(api, timeline, task):
-	listId = task['list']['id']
-	taskSeriesId = task['list']['taskseries'][0]['id']
-	taskId = task['list']['taskseries'][0]['task'][0]['id']
+	listId = task.list.id
+	taskSeriesId = task.list.taskseries[0].id
+	taskId = task.list.taskseries[0].task[0].id
 
-	api.TasksAddTags(timeline, listId, taskSeriesId, taskId, 'tag1')
-	assert 'tag1' in {x['name'] for x in api.TagsGetList()}
-	allTaskSeries = api.TasksGetList(list_id=listId)['list'][0]['taskseries']
-	assert len(allTaskSeries) == 1, allTaskSeries
-	assert {'tag1'} == set(allTaskSeries[0]['tags']['tag']), allTaskSeries
+	api.TasksAddTags(timeline, listId, taskSeriesId, taskId, ['tag1'])
+	assert 'tag1' in {x.name for x in api.TagsGetList().tags.tag}
+	# allTaskSeries = api.TasksGetList(list_id=listId).tasks.list[0].id
+	# api.TasksDelete(
+	# 	timeline, task.list.id,
+	# 	task.list.taskseries[0].id,
+	# 	task.list.taskseries[0].task[0].id)
+	# assert len(allTaskSeries) == 1, allTaskSeries
+	# assert {'tag1'} == set(allTaskSeries[0]['tags']['tag']), allTaskSeries
 
-	api.TasksAddTags(timeline, listId, taskSeriesId, taskId, 'tag2,tag3')
-	assert {'tag1', 'tag2', 'tag3'} <= {x['name'] for x in api.TagsGetList()}
-	allTaskSeries = api.TasksGetList(list_id=listId)['list'][0]['taskseries']
-	assert len(allTaskSeries) == 1, allTaskSeries
-	assert {'tag1', 'tag2', 'tag3'} == set(allTaskSeries[0]['tags']['tag']), allTaskSeries
+	# api.TasksAddTags(timeline, listId, taskSeriesId, taskId, 'tag2,tag3')
+	# assert {'tag1', 'tag2', 'tag3'} <= {x['name'] for x in api.TagsGetList()}
+	# allTaskSeries = api.TasksGetList(list_id=listId)['list'][0]['taskseries']
+	# assert len(allTaskSeries) == 1, allTaskSeries
+	# assert {'tag1', 'tag2', 'tag3'} == set(allTaskSeries[0]['tags']['tag']), allTaskSeries
 
-	api.TasksRemoveTags(timeline, listId, taskSeriesId, taskId, 'tag1,tag3')
-	assert {'tag1', 'tag2', 'tag3'} <= {x['name'] for x in api.TagsGetList()}
-	allTaskSeries = api.TasksGetList(list_id=listId)['list'][0]['taskseries']
-	assert len(allTaskSeries) == 1, allTaskSeries
-	assert {'tag2'} == set(allTaskSeries[0]['tags']['tag']), allTaskSeries
+	# api.TasksRemoveTags(timeline, listId, taskSeriesId, taskId, 'tag1,tag3')
+	# assert {'tag1', 'tag2', 'tag3'} <= {x['name'] for x in api.TagsGetList()}
+	# allTaskSeries = api.TasksGetList(list_id=listId)['list'][0]['taskseries']
+	# assert len(allTaskSeries) == 1, allTaskSeries
+	# assert {'tag2'} == set(allTaskSeries[0]['tags']['tag']), allTaskSeries
 
-	api.TasksSetTags(timeline, listId, taskSeriesId, taskId, tags='tag3')
-	allTaskSeries = api.TasksGetList(list_id=listId)['list'][0]['taskseries']
-	assert len(allTaskSeries) == 1, allTaskSeries
-	assert {'tag3'} == set(allTaskSeries[0]['tags']['tag']), allTaskSeries
+	# api.TasksSetTags(timeline, listId, taskSeriesId, taskId, tags='tag3')
+	# allTaskSeries = api.TasksGetList(list_id=listId)['list'][0]['taskseries']
+	# assert len(allTaskSeries) == 1, allTaskSeries
+	# assert {'tag3'} == set(allTaskSeries[0]['tags']['tag']), allTaskSeries
 
 def test_dates(api, timeline, task):
-	listId = task['list']['id']
-	taskSeriesId = task['list']['taskseries'][0]['id']
-	taskId = task['list']['taskseries'][0]['task'][0]['id']
+	listId = task.list.id
+	taskSeriesId = task.list.taskseries[0].id
+	taskId = task.list.taskseries[0].task[0].id
 
 	settings = api.SettingsGetList()
-	userTimezone = gettz(settings['timezone'])
+	userTimezone = gettz(settings.settings.timezone)
 
 	dueDate = datetime(2021, 6, 1, 0, 0, 0, tzinfo=userTimezone)
 
 	updatedTask = api.TasksSetDueDate(timeline, listId, taskSeriesId, taskId, due=dueDate.isoformat())
-	assert updatedTask['list']['taskseries'][0]['task'][0]['due'] == dueDate.astimezone(timezone.utc).isoformat()[:19] + 'Z'
+	assert updatedTask.list.taskseries[0].task[0].due == dueDate.astimezone(timezone.utc).isoformat()[:19] + 'Z'
 
 	startDate = datetime(2021, 5, 1, 0, 0, 0, tzinfo=userTimezone)
 	updatedTask = api.TasksSetStartDate(timeline, listId, taskSeriesId, taskId, start=startDate.isoformat())
-	assert updatedTask['list']['taskseries'][0]['task'][0]['start'] == startDate.astimezone(timezone.utc).isoformat()[:19] + 'Z'
+	assert updatedTask.list.taskseries[0].task[0].start == startDate.astimezone(timezone.utc).isoformat()[:19] + 'Z'
 
 def test_get_list(api):
-	response = api.TasksGetList()
-	info([x['id'] for x in response['list']])
-	for list_ in response['list']:
-		info(f'List: {list_["id"]}')
-		if 'taskseries' in list_:
-			info(f'taskseries count: {len(list(list_["taskseries"]))}')
-		else:
-			info(f'task: {list_["id"]}')
+	allTasks = api.TasksGetList()
+	if allTasks.tasks.list is None:
+		return
+	info([x.id for x in allTasks.tasks.list])
+	for list_ in allTasks.tasks.list:
+		info(f'List ID: {list_.id}')
+
+def test_lists_get_list(api):
+	allLists = api.ListsGetList()
+	listNames = [x.name for x in allLists.lists.list]
+	assert 'Inbox' in listNames, listNames
+	assert 'Sent' in listNames, listNames
+	assert 'Personal' in listNames, listNames
+	assert 'Work' in listNames, listNames
