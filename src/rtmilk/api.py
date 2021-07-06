62e34308873f2d5cd7a90e32d9a077b76f2328e8
Rehan Khwaja <rehan@khwaja.name>
2021-07-05 22:21:12 -0700

    Use date/datetime objects as arguments to TasksSetStartDate and TasksSetDueDate

--- src/rtmilk/api.py
+++ src/rtmilk/api.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from hashlib import md5
 from logging import getLogger
 from pprint import pformat
@@ -30,6 +31,10 @@ def _CheckKwargs(keywordArgs, allowedArgs):
 		if name not in allowedArgs:
 			raise TypeError(f'{name} is an unexpected keyword argument')
 
+# Serialize python datetime object to string for use by main "set" API functions
+def _RtmDate(date_):
+	return datetime(date_.year, date_.month, date_.day, hour=8).isoformat()
+
 def _ValidateReturn(type_, rsp):
 	try:
 		_log.info(f'Parsing {type_}:\n{pformat(rsp)}')
@@ -59,7 +64,7 @@ class API:
 		self.storage = storage
 
 	@validate_arguments
-	def SetToken(self, token: str):
+	def SetToken(self, token: str) -> None:
 		self.storage.Save(token)
 
 	def _ApiSig(self, params):
@@ -85,84 +90,84 @@ class API:
 		params.update({'auth_token': self.storage.Load()})
 		return self._Call(params)
 
-	def TestEcho(self, **params):
+	def TestEcho(self, **params) -> EchoResponse:
 		_log.info(f'Echo: {params}')
 		rsp = self._CallUnauthorized('rtm.test.echo', **params)
 		return EchoResponse(**rsp)
 
-	def AuthGetFrob(self):
+	def AuthGetFrob(self) -> str:
 		rsp = self._CallUnauthorized('rtm.auth.getFrob')
 		return rsp['frob']
 
 	@validate_arguments
-	def AuthGetToken(self, frob: str):
+	def AuthGetToken(self, frob: str) -> str:
 		rsp = self._CallUnauthorized('rtm.auth.getToken', frob=frob)
 		return rsp['auth']['token']
 
 	@validate_arguments
-	def AuthCheckToken(self, auth_token: str):
+	def AuthCheckToken(self, auth_token: str) -> AuthResponse:
 		rsp = self._CallUnauthorized('rtm.auth.checkToken', auth_token=auth_token)
 		return AuthResponse(**rsp)
 
 	@validate_arguments
-	def ListsAdd(self, timeline: str, name: str, **kwargs):
+	def ListsAdd(self, timeline: str, name: str, **kwargs) -> ListResponse:
 		_CheckKwargs(kwargs, ['filter']) # TODO validate parameter
 		rsp = self._CallAuthorized('rtm.lists.add', timeline=timeline, name=name, **kwargs)
 		return ListResponse(**rsp)
 
 	@validate_arguments
-	def ListsArchive(self, timeline: str, list_id: str):
+	def ListsArchive(self, timeline: str, list_id: str) -> ListResponse:
 		rsp = self._CallAuthorized('rtm.lists.archive', timeline=timeline, list_id=list_id)
 		return ListResponse(**rsp)
 
 	@validate_arguments
-	def ListsDelete(self, timeline: str, list_id: str):
+	def ListsDelete(self, timeline: str, list_id: str) -> ListResponse:
 		rsp = self._CallAuthorized('rtm.lists.delete', timeline=timeline, list_id=list_id)
 		return ListResponse(**rsp)
 
-	def ListsGetList(self):
+	def ListsGetList(self) -> ListResponse:
 		rsp = self._CallAuthorized('rtm.lists.getList')
 		return ListsResponse(**rsp)
 
 	@validate_arguments
-	def ListsSetDefaultList(self, timeline: str, list_id: str):
+	def ListsSetDefaultList(self, timeline: str, list_id: str) -> ListResponse:
 		rsp = self._CallAuthorized('rtm.lists.setDefaultList', timeline=timeline, list_id=list_id)
 		return ListResponse(**rsp)
 
 	@validate_arguments
-	def ListsSetName(self, timeline: str, list_id: str, name: str):
+	def ListsSetName(self, timeline: str, list_id: str, name: str) -> ListResponse:
 		rsp = self._CallAuthorized('rtm.lists.setName', timeline=timeline, list_id=list_id, name=name)
 		return ListResponse(**rsp)
 
 	@validate_arguments
-	def ListsUnarchive(self, timeline: str, list_id: str):
+	def ListsUnarchive(self, timeline: str, list_id: str) -> ListResponse:
 		rsp = self._CallAuthorized('rtm.lists.unarchive', timeline=timeline, list_id=list_id)
 		return ListResponse(**rsp)
 
-	def PushGetSubscriptions(self):
+	def PushGetSubscriptions(self) -> dict:
 		return self._CallAuthorized('rtm.push.getSubscriptions')['subscriptions']
 
-	def PushGetTopics(self):
+	def PushGetTopics(self) -> dict:
 		return self._CallAuthorized('rtm.push.getTopics')['topics']
 
 	@validate_arguments
-	def PushSubscribe(self, url: AnyHttpUrl, topics: List[str], push_format, timeline: str, **kwargs):
+	def PushSubscribe(self, url: AnyHttpUrl, topics: List[str], push_format, timeline: str, **kwargs) -> dict:
 		_CheckKwargs(kwargs, ['lease_seconds', 'filter']) # TODO validate parameters
 		return self._CallAuthorized('rtm.push.subscribe', url=url, topics=topics, push_format=push_format, timeline=timeline, **kwargs)
 
 	@validate_arguments
-	def PushUnsubscribe(self, timeline: str, subscription_id: str):
+	def PushUnsubscribe(self, timeline: str, subscription_id: str) -> dict:
 		self._CallAuthorized('rtm.push.unsubscribe', timeline=timeline, subscription_id=subscription_id)
 
-	def TimelinesCreate(self):
+	def TimelinesCreate(self) -> TimelineResponse:
 		rsp = self._CallAuthorized('rtm.timelines.create')
 		return TimelineResponse(**rsp)
 
-	def SettingsGetList(self):
+	def SettingsGetList(self) -> SettingsResponse:
 		rsp = self._CallAuthorized('rtm.settings.getList')
 		return SettingsResponse(**rsp)
 
-	def TagsGetList(self):
+	def TagsGetList(self) -> TagListResponse:
 		rsp = self._CallAuthorized('rtm.tags.getList')
 		try:
 			return TagListResponse(**rsp)
@@ -171,64 +176,66 @@ class API:
 			raise RTMError(failStat.err.code, failStat.err.msg) from e
 
 	@validate_arguments
-	def TasksAdd(self, timeline: str, name: str, **kwargs):
+	def TasksAdd(self, timeline: str, name: str, **kwargs) -> TaskResponse:
 		_CheckKwargs(kwargs, ['list_id', 'parse', 'parent_task_id', 'external_id']) # TODO validate parameters
 		rsp = self._CallAuthorized('rtm.tasks.add', timeline=timeline, name=name, **kwargs)
 		return _ValidateReturn(TaskResponse, rsp)
 
 	@validate_arguments
-	def TasksAddTags(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, tags: List[str]):
+	def TasksAddTags(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, tags: List[str]) -> TaskResponse:
 		# TODO the join here should be done as part of the pydantic parameter validation stuff
 		rsp = self._CallAuthorized('rtm.tasks.addTags', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, tags=','.join(tags))
 		return _ValidateReturn(TaskResponse, rsp)
 
 	@validate_arguments
-	def TasksComplete(self, timeline: str, list_id: str, taskseries_id: str, task_id: str):
+	def TasksComplete(self, timeline: str, list_id: str, taskseries_id: str, task_id: str) -> TaskResponse:
 		rsp = self._CallAuthorized('rtm.tasks.complete', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id)
 		return _ValidateReturn(TaskResponse, rsp)
 
 	@validate_arguments
-	def TasksDelete(self, timeline: str, list_id: str, taskseries_id: str, task_id: str):
+	def TasksDelete(self, timeline: str, list_id: str, taskseries_id: str, task_id: str) -> TaskResponse:
 		rsp = self._CallAuthorized('rtm.tasks.delete', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id)
 		return _ValidateReturn(TaskResponse, rsp)
 
-	def TasksGetList(self, **kwargs):
+	def TasksGetList(self, **kwargs) -> ListResponse:
 		_CheckKwargs(kwargs, ['list_id', 'filter', 'last_sync']) # TODO validate parameters
 		rsp = self._CallAuthorized('rtm.tasks.getList', **kwargs)
 		return _ValidateReturn(ListResponse, rsp)
 
 	@validate_arguments
-	def TasksMovePriority(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, direction: PriorityDirectionEnum):
+	def TasksMovePriority(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, direction: PriorityDirectionEnum) -> TaskResponse:
 		direction = direction.value
 		rsp = self._CallAuthorized('rtm.tasks.movePriority', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, direction=direction)
 		return _ValidateReturn(TaskResponse, rsp)
 
 	@validate_arguments
-	def TasksNotesAdd(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, note_title: str, note_text: str):
+	def TasksNotesAdd(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, note_title: str, note_text: str) -> NotesResponse:
 		rsp = self._CallAuthorized('rtm.tasks.notes.add', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, note_title=note_title, note_text=note_text)
 		return _ValidateReturn(NotesResponse, rsp)
 
 	@validate_arguments
-	def TasksRemoveTags(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, tags: List[str]):
+	def TasksRemoveTags(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, tags: List[str]) -> TaskResponse:
 		tags = ','.join(tags)
 		rsp = self._CallAuthorized('rtm.tasks.removeTags', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, tags=tags)
 		return _ValidateReturn(TaskResponse, rsp)
 
 	@validate_arguments
-	def TasksSetDueDate(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, **kwargs):
+	def TasksSetDueDate(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, **kwargs) -> TaskResponse:
 		_CheckKwargs(kwargs, ['due', 'has_due_time', 'parse']) # TODO validate parameters
 		if 'has_due_time' in kwargs:
 			kwargs['has_due_time'] = '1' if kwargs['has_due_time'] else '0'
+		if 'due' in kwargs:
+			kwargs['due'] = _RtmDate(kwargs['due'])
 		rsp = self._CallAuthorized('rtm.tasks.setDueDate', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, **kwargs)
 		return _ValidateReturn(TaskResponse, rsp)
 
 	@validate_arguments
-	def TasksSetName(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, name: str):
+	def TasksSetName(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, name: str) -> TaskResponse:
 		rsp = self._CallAuthorized('rtm.tasks.setName', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, name=name)
 		return _ValidateReturn(TaskResponse, rsp)
 
 	@validate_arguments
-	def TasksSetPriority(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, **kwargs):
+	def TasksSetPriority(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, **kwargs) -> TaskPayload:
 		_CheckKwargs(kwargs, ['priority']) # TODO validate parameter
 		if 'priority' in kwargs:
 			kwargs['priority'] = kwargs['priority'].value
@@ -236,15 +243,17 @@ class API:
 		return _ValidateReturn(TaskPayload, rsp)
 
 	@validate_arguments
-	def TasksSetStartDate(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, **kwargs):
+	def TasksSetStartDate(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, **kwargs) -> TaskResponse:
 		_CheckKwargs(kwargs, ['start', 'has_start_time', 'parse']) # TODO validate parameter
 		if 'has_start_time' in kwargs:
 			kwargs['has_start_time'] = '1' if kwargs['has_start_time'] else '0'
+		if 'start' in kwargs:
+			kwargs['start'] = _RtmDate(kwargs['start'])
 		rsp = self._CallAuthorized('rtm.tasks.setStartDate', timeline=timeline, list_id=list_id, taskseries_id=taskseries_id, task_id=task_id, **kwargs)
 		return _ValidateReturn(TaskResponse, rsp)
 
 	@validate_arguments
-	def TasksSetTags(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, **kwargs):
+	def TasksSetTags(self, timeline: str, list_id: str, taskseries_id: str, task_id: str, **kwargs) -> TaskResponse:
 		_CheckKwargs(kwargs, ['tags']) # TODO validate parameter
 		if 'tags' in kwargs:
 			kwargs['tags'] = ','.join(kwargs['tags'])
