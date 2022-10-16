6dc6331ea0f786f7e0979bfbc693df769493d75d
Rehan Khwaja <rehan@khwaja.name>
2022-10-15 18:51:16 -0700

    Add null handler
    https://docs.python.org/3/howto/logging.html#configuring-logging-for-a-library

--- src/rtmilk/__init__.py
+++ src/rtmilk/__init__.py
@@ -1,5 +1,9 @@
+from logging import getLogger, NullHandler
+
 from .api_sync import *
 from .api_async import *
 from .authorization import *
 from .models import *
 from .client import *
+
+getLogger(__name__).addHandler(NullHandler())
