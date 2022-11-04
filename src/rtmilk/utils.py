3419a1781b5488b89f514886d79fe3c48ad938f7
anekos <anekos@snca.net>
2022-11-04 16:43:23 +0900

    EmptyStrToNone supports `AnyHttpUrl`

--- src/rtmilk/utils.py
+++ src/rtmilk/utils.py
@@ -2,6 +2,7 @@ from datetime import datetime, timezone
 from typing import Generic, Optional, TypeVar
 
 from pydantic.fields import ModelField # pylint: disable=no-name-in-module
+from pydantic.networks import AnyHttpUrl # pylint: disable=no-name-in-module
 
 PydanticField = TypeVar('PydanticField')
 
@@ -15,4 +16,9 @@ class EmptyStrToNone(Generic[PydanticField]):
 	def validate(cls, v: PydanticField, field: ModelField) -> Optional[PydanticField]: # pylint: disable=unused-argument
 		if v == '':
 			return None
-		return datetime.strptime(v, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
+		type_ = field.sub_fields[0].type_
+		if isinstance(type_, datetime):
+			return datetime.strptime(v, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
+		if isinstance(type_, AnyHttpUrl):
+			return AnyHttpUrl(v)
+		return v
