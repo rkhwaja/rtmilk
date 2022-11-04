6ce74afde91c7149ba137c6586420ac8d99160da
Rehan Khwaja <rehan@khwaja.name>
2022-11-04 09:20:42 -0600

    Fix tests failing on dates and add test for url

--- src/rtmilk/utils.py
+++ src/rtmilk/utils.py
@@ -2,7 +2,6 @@ from datetime import datetime, timezone
 from typing import Generic, Optional, TypeVar
 
 from pydantic.fields import ModelField # pylint: disable=no-name-in-module
-from pydantic.networks import AnyHttpUrl # pylint: disable=no-name-in-module
 
 PydanticField = TypeVar('PydanticField')
 
@@ -13,12 +12,11 @@ class EmptyStrToNone(Generic[PydanticField]):
 		yield cls.validate
 
 	@classmethod
-	def validate(cls, v: PydanticField, field: ModelField) -> Optional[PydanticField]: # pylint: disable=unused-argument
+	def validate(cls, v: PydanticField, field: ModelField) -> Optional[PydanticField]:
 		if v == '':
 			return None
-		type_ = field.sub_fields[0].type_
-		if isinstance(type_, datetime):
+
+		if field.sub_fields[0].type_ == datetime:
 			return datetime.strptime(v, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
-		if isinstance(type_, AnyHttpUrl):
-			return AnyHttpUrl(v)
+
 		return v
