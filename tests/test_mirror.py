3b0976e6fbc76060646f35a4c93e840cdc056b35
Rehan Khwaja <rehan@khwaja.name>
2024-06-02 18:46:58 -0700

    Use same tag names for tests

--- tests/test_mirror.py
+++ tests/test_mirror.py
@@ -9,7 +9,7 @@ def test_ChangeTagsOnTaskBeforeStart(client, taskCreator):
 	name = 'rtmilk test ChangeTagsOnTaskBeforeStart'
 	task = taskCreator.Add(name)
 	task.startDate.Set(datetime.today() + timedelta(days=2))
-	task.tags.Set({'rtm-test-tag1', 'rtm-test-tag2'})
+	task.tags.Set({'rtmilk-test-tag1', 'rtmilk-test-tag2'})
 	task.complete.Set(True)
 
 	tasks = client.Get(f'name: {name}')
@@ -17,9 +17,9 @@ def test_ChangeTagsOnTaskBeforeStart(client, taskCreator):
 	task = tasks[0]
 
 	taskData = TaskData.FromTask(task)
-	taskData.tags -= {'rtm-test-tag1'}
+	taskData.tags -= {'rtmilk-test-tag1'}
 	Mirror(client, [task], [taskData])
 
 	tasks = client.Get(f'name: {name}')
 	assert len(tasks) == 1
-	assert tasks[0].tags.value == {'rtm-test-tag2'}
+	assert tasks[0].tags.value == {'rtmilk-test-tag2'}
